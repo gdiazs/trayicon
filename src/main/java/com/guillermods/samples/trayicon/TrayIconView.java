afbb9c694ff327c62e4cfa97d71ca47c77f8b0f2
Guillermo Bernardo Diaz Solis <gdiazs@cri.CORP.REDBAC.com>
2016-11-10 14:22:06 -0600

    Agregado lock file para validar ejecución del mismo

--- src/main/java/com/guillermods/samples/trayicon/TrayIconView.java
+++ src/main/java/com/guillermods/samples/trayicon/TrayIconView.java
@@ -6,11 +6,22 @@ import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Clase principal
+ * 
+ * @author gdiazs
+ *
+ */
 public class TrayIconView implements Runnable {
+
+	private static final String LOCK_FILEPATH = System.getProperty("java.io.tmpdir") + File.separator + "bac.lock";
+	private static final File lock = new File(LOCK_FILEPATH);
+	private static boolean locked = false;
 
 	private TrayIcon trayIcon;
 
@@ -20,6 +31,9 @@ public class TrayIconView implements Runnable {
 
 	private MenuItem closeItem;
 
+	/**
+	 * Se crea un hilo para mostrar el ícono
+	 */
 	public void run() {
 		if (SystemTray.isSupported()) {
 			try {
@@ -52,10 +66,38 @@ public class TrayIconView implements Runnable {
 	}
 
 	public static void main(String args[]) {
+		try {
+			if (lock()) {
+				Thread thread = new Thread(new TrayIconView());
+				thread.start();
+			} else {
+				System.exit(0);
+			}
+		} catch (IOException e) {
 
+			e.printStackTrace();
+		}
 
-		Thread thread = new Thread(new TrayIconView());
-		thread.start();
+	}
+
+	/**
+	 * Crea un lock file para validar si la applicación se encuentra en
+	 * ejecución
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean lock() throws IOException {
+		if (locked)
+			return true;
+
+		if (lock.exists())
+			return false;
+
+		lock.createNewFile();
+		lock.deleteOnExit();
+		locked = true;
+		return true;
 	}
 
 }
